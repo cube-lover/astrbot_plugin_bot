e8f03231597894c7b8853c1379e501d397e66a7e
cube <2267154199>
2025-10-01 15:48:57 +0800

    一键保存本地

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# astrbot_plugin_bot
+# AstrBot beautyvideo plugin
+
+> 是一款QQ随机美女视频的AstrBot插件
+
+---
+
+欢迎使用我的机器人哦😘[点我查看](https://qun.qq.Com/qunpro/robot/qunshare?biz_type=1&robot_uin=3889263267)
+
+---
+
+
+## 支持功能：
+
+- 随机一个美女视频
+
+---
+
+## 安装方法：
+
+复制链接 到astr webui端插件页面 使用链接安装即可
+
+```
+https://github.com/ovoox/zhiyu-astrbot-beautyvideo
+```
+
+---
+
+## 使用方法：
+
+##### 相关指令：
+
+* 美女视频
+* 看视频
+
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-13245
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.star import Context, Star, register
+from astrbot.api.message_components import Plain, Video 
+import aiohttp
+import base64
+
+@register("beauty_video", "美女视频", "获取美女视频的插件", "1.0")
+class BeautyVideoPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+        self.encrypted_api = "aHR0cHM6Ly9hcGkuczAxcy5jbi9BUEkvbXZzcC8="
+        self.session = aiohttp.ClientSession()
+    async def terminate(self):
+        await self.session.close()
+    def _decrypt_api(self):
+        return base64.b64decode(self.encrypted_api).decode()
+    @filter.regex(r"^[/]?(美女视频|看视频|看美女)$")
+    async def get_beauty_video(self, event: AstrMessageEvent):
+        try:
+            real_api_url = self._decrypt_api()
+            
+            async with self.session.get(real_api_url) as response:
+                if response.status == 200:
+                    video_component = Video.fromURL(real_api_url)
+                    yield event.chain_result([video_component])
+                else:
+                    yield event.plain_result("获取视频失败 请稍后重试")
+
+        except Exception:
+            yield event.plain_result("视频异常 请稍后重试")
